f5c185f2efa42727cad83795493f6e43d6bd8d15
Srivats-Srihari <ss.astrofacts@gmail.com>
2025-09-16 16:02:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -302,3 +302,17 @@ if __name__ == "__main__":
         print("Shutting down bot...")
     finally:
         engine.quit()
+from flask import Flask
+import os
+
+# create a simple flask app
+server = Flask(__name__)
+
+@server.route("/")
+def home():
+    return "Underfish is alive!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    server.run(host="0.0.0.0", port=port)
+
